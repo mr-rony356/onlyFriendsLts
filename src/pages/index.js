88bc01beef26c35be6d5ebd2f717e6844e526ef1
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-09-16 16:08:31 +0600

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,6 @@ function HomePage({ user, attributes, initialAds, premiumAds }) {
   const { api } = useApi();
   const [ads, setAds] = useState(initialAds);
   const [isCookiesPopupOpen, setIsCookiesPopupOpen] = useState(false);
-  const [isFilterVisible, setIsFilterVisible] = useState(false);
   const [filters, setFilters] = useState({
     regions: [],
     tags: [],
@@ -53,61 +52,23 @@ function HomePage({ user, attributes, initialAds, premiumAds }) {
   });
   const [activeType, setActiveType] = useState(0);
 
-  // Fetch ads based on activeType when it changes
   const fetchAds = async (tab) => {
     setActiveType(tab);
-
     const res = await api.fetchAds(tab);
     if (res.err) setAds([]);
     else setAds(res);
   };
 
   useEffect(() => {
-    const handleResize = () => {
-      setIsFilterVisible(!window.matchMedia("(max-width: 820px)").matches);
-    };
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
     if (!Cookies.get("cookiesPopupShown")) {
       setIsCookiesPopupOpen(true);
       Cookies.set("cookiesPopupShown", true);
     }
-  }, [isCookiesPopupOpen]);
-
-  const toggleFilter = () => {
-    setIsFilterVisible(!isFilterVisible);
-  };
+  }, []);
 
   return (
     <>
-      <Head>
-        <title>
-          Erotische Anzeigen für Sexkontakte und Onlyfans Accounts in der
-          Schweiz - Die besten Sex & Erotik Anzeigen der Schweiz: Für jeden
-          Geschmack! onlyfriend.ch ▷ Das Schweizer Sex & Erotik Inserate Portal.
-        </title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta
-          name="description"
-          content="Entdecken Sie auf unserer Webseite erotische Anzeigen für Sexkontake und Onlyfans Accounts in der Schweiz. Treffen Sie heiße Girls in Ihrer Nähe und erleben Sie prickelnde Abenteuer. Ohne Anmeldung können Sie direkt mit den Girls in Kontakt kommen."
-        />
-        <meta
-          name="keywords"
-          content="Erotische Anzeigen, Sex in Zürich, Blowjob in Zürich, Escort in Zürich, Gangbang in Zürich, Girlfriend Sex in Zürich, Striptease in Zürich, Sex in Aargau, Blowjob in Aargau, Escort in Aargau, Gangbang in Aargau, Girlfriend Sex in Aargau, Striptease in Aargau, Sex in Luzern, Blowjob in Luzern, Escort in Luzern, Gangbang in Luzern, Girlfriend Sex in Luzern, Striptease in Bern, Sex in Bern, Blowjob in Bern, Escort in Bern, Gangbang in Bern, Girlfriend Sex in Bern, Striptease in Bern, Sex in Basel, Blowjob in Basel, Escort in Basel, Gangbang in Basel, Girlfriend Sex in Basel, Striptease in Basel, Junge Frauen, Sexy Latinas, Escort, Sexy Studentin, Milf, Sextreffen, Webcam, Sexchat, Sexting, Cam2Cam, Erotik-Kleinanzeigen, Sexkontakte, Begleitservice, Callgirls, Escortservice, Erotische Massagen, Fetisch-Anzeigen, BDSM-Kontakte, Sexpartys, Swinger-Kontakte, Erotikjobs, Erotik-Shops, Webcam-Shows, Adult-Dating, Dominas, Bordell-Inserate, Stripper-Inserate, TS-Inserate, Onlyfans, Onlyfriends,"
-        />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="mobile-web-app-capable" content="yes" />
-      </Head>
+      <Head>{/* ... (keep the existing Head content) */}</Head>
 
       <div className="page page--home">
         <h1 className="home__title">
@@ -115,13 +76,11 @@ function HomePage({ user, attributes, initialAds, premiumAds }) {
         </h1>
         <div className="home__content">
           <div className="home__left">
-            {isFilterVisible && (
-              <FilterForm
-                filters={filters}
-                setFilters={setFilters}
-                attributes={attributes}
-              />
-            )}
+            <FilterForm
+              filters={filters}
+              setFilters={setFilters}
+              attributes={attributes}
+            />
           </div>
           <div className="home__right">
             <div className="button--inline">
@@ -146,7 +105,6 @@ function HomePage({ user, attributes, initialAds, premiumAds }) {
                 height={500}
                 alt="filter"
                 className="filter"
-                onClick={toggleFilter}
               />
             </div>
             <AdList
@@ -161,63 +119,7 @@ function HomePage({ user, attributes, initialAds, premiumAds }) {
             )}
           </div>
         </div>
-        <div>
-          <br />
-          <br />
-          <h3>
-            Sextreffen in der Schweiz: die besten Sexinserate und Kontakte auf
-            onlyfriend.ch - Onlyfriends
-          </h3>
-          <p>
-            Onlyfriends ist ein Erotikportal, wo frei arbeitende Frauen ein
-            Inserat erstellen und mit Kunden in Kontakt treten. Onlyfriends
-            verfügt auch eine Verlinkung zu Onlyfans. Wenn man unter der Rubrik
-            Onlyfans Inserate sucht, erscheinen Inserate, die eine direkte
-            Verlinkung zu Ihrem Onlyfans Account haben. Onlyfriends bezieht sich
-            somit auf Sex und Erotik Inserate und auch auf Onlyfans Inserate.
-            Onlyfriends überprüft jedes Inserat nach Ihrer Echtheit und bei
-            doppelter Überprüfung werden die Inserate noch verifiziert. Hierbei
-            will Onlyfriends vermeiden, dass gefälschte Inserate erstellt
-            werden. Du möchtest heisse Sextreffen in der Schweiz erleben? Bei
-            uns findest du die besten Sexkontakte und Erotikanzeigen. Egal,
-            welche Wünsche oder Neigungen du ausleben möchtest – bei
-            onlyfriend.ch (Onlyfriends) ist für jeden Geschmack etwas dabei.
-            Wähle aus verschiedenen Kategorien deine Vorliebe und finde private
-            Sexkontakte.
-          </p>
-          <br />
-          <br />
-          <h3>Sexinserate für jeden Geschmack</h3>
-          <p>
-            Onlyfriends verfügt über ein breites Angebot an Services: Escort
-            Service, Sextreffen, Erotik Massagen, Sex Beziehungen, Onlyfans
-            Benutzer, Sexpartys, Callgirls, Erotikjobs und vieles mehr! Suchst
-            du nach Escort Service in Zürich, Sextreffen in Aargau, Erotik
-            Massage in Winterthur oder Sugar Babe treffen. Vielleicht suchst du
-            nach einem Seriösen Begleitservice oder eine langfristige
-            Sexbeziehung in der Schweiz. Onlyfriends verfügt auch über Adult
-            Dating, Fetisch-Anzeigen und BDSM-Kontakte.
-            <br />
-            <br />
-            Für sie, Erotikjobs <br /> Du wünschst dir einen dominanten Mann mit
-            grossem Schwanz und Sixpack? Auf onlyfriend.ch - Onlyfriends findest
-            du garantiert den passenden Sexpartner.
-          </p>
-          <br />
-          <br />
-          <h3>Sex und Escort in der Schweiz</h3>
-          <p>
-            Von Zürich über Aargau bis hin zu Zug – bei uns findest du
-            Sexanzeigen von willigen Frauen in der gesamten Schweiz. So kannst
-            du dich ganz nach deinen Wünschen befriedigen lassen – sowohl bei
-            dir zu Hause als auch bei einem Sexpartner in deiner Nähe. Sexy
-            Girls lassen deine Erotikträume wahr werden. In unserem Schweizer
-            Sexforum kannst du dich ausserdem mit anderen Mitgliedern
-            austauschen und erfährst alles über die besten Sexclubs und Escorts
-            in deiner Region. Bestelle die schärfsten Sexpartner zu dir nach
-            Hause oder besuche sie ganz privat für geilen Sex in der Schweiz.
-          </p>
-        </div>
+        {/* ... (keep the existing content below) */}
       </div>
     </>
   );
