06c93520146c44a018e6ffc934f7fc2087dfb2b0
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-09-17 00:56:26 +0600

    missing content added

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
-    defaultLocale: "en",
+    defaultLocale: "de",
     locales: ["en", "de"],
   },
   localePath:
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="de">
         <Head>
           <link rel="icon" href="/favicon.png" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
